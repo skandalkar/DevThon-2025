dde6330636f24e1d9b5faf54229a1d25bcaeebcb
Santosh Kandalkar <skandalkar1508@gmail.com>
2025-03-18 21:39:01 +0530

    Connection with database.

---
+++ back-end/config/dbConnection.js
@@ -0,0 +1,21 @@
+
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+        console.log("Database connected successfully.");
+    } catch (error) {
+        console.log("Database connection failed.");
+        process.exit(1);
+    }
+};
+
+module.exports = dbConnection;
