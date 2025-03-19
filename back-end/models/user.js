640e35306341c0840cab00a11312edae852eeebe
Santosh Kandalkar <skandalkar1508@gmail.com>
2025-03-20 03:38:31 +0530

    Modified with single property.

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    contact: { type: String, required: true },
+    contact: { type: String },  //removed required
     role: { type: String, default: "Principal" },
 }, options);
 
