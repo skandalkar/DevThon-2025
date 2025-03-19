c664ca787ee7884b121d2f0c8ee7a991fd9c1357
Santosh Kandalkar <skandalkar1508@gmail.com>
2025-03-20 01:19:40 +0530

    Can use to generate JWT token.

--- back-end/utils/generateToken.js
+++ back-end/utils/generateToken.js
@@ -1,12 +1,19 @@
 
 
 // Utility function to generate JWT token
+
 const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = "your_secret_key"; // Use environment variable in production
-
 const generateToken = (user, role) => {
-    return jwt.sign({ id: user._id, role }, JWT_SECRET, { expiresIn: "1h" });
+    if (!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET is not defined in environment variables.");
+    }
+
+    return jwt.sign(
+        { id: user._id, email: user.email, role },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" } // Token expires in 1 hr
+    );
 };
 
 module.exports = generateToken;
