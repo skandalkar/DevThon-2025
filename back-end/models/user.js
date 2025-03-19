7cc39e66566f90b60d5a8dbcd8b1c7ef35c6249e
Santosh Kandalkar <skandalkar1508@gmail.com>
2025-03-19 23:45:55 +0530

    Modification of user schema defination.

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -1,52 +1,7 @@
 //this file contains the schema for the user based on their role
-
 /*
     This file (`user.js`) defines Mongoose schemas for different user roles (Principal, HoD, Faculty, and Student) in a system. It specifies their required fields and creates models for database storage and retrieval.
 */
-
-/*
-const mongoose = require("mongoose");
-//schema for Pricipal
-const principalSchema = new mongoose.Schema({
-    name: { type: String },
-    email: { type: String, required: true, unique: true },
-    contact: { type: Number, required: true, unique: true },
-    password: { type: String, required: true }
-});
-//schema for Head of Department
-const hodSchema = new mongoose.Schema({
-    name: { type: String },
-    email: { type: String, required: true, unique: true },
-    contact: { type: Number, required: true, unique: true },
-    department: { type: String, required: true },
-    password: { type: String, required: true }
-});
-//schema for Faculty
-const facultySchema = new mongoose.Schema({
-    name: { type: String },
-    email: { type: String, required: true, unique: true },
-    contact: { type: Number, required: true, unique: true },
-    department: { type: String, required: true },
-    designation: { type: String, required: true },
-    password: { type: String, required: true }
-});
-//schema for Student
-const studentSchema = new mongoose.Schema({
-    name: { type: String },
-    email: { type: String, required: true, unique: true },
-    contact: { type: Number, required: true, unique: true },
-    department: { type: String, required: true },
-    password: { type: String, required: true },
-    roll_number: { type: String, required: true, unique: true },
-    year: { type: Number, required: true }
-});
-const Principal = mongoose.model("Principal", principalSchema);
-const HoD = mongoose.model("HoD", hodSchema);
-const Faculty = mongoose.model("Faculty", facultySchema);
-const Student = mongoose.model("Student", studentSchema);
-module.exports = { Principal, HoD, Faculty, Student };
-*/
-
 
 //new updated schema for user
 const mongoose = require("mongoose");
@@ -86,4 +41,3 @@ const Student = User.discriminator("Student", new mongoose.Schema({
 }, options));
 
 module.exports = { User, Principal, HoD, Faculty, Student };
-
