100e282bc297629b4c6da43a434aa534d770e89b
Santosh Kandalkar <skandalkar1508@gmail.com>
2025-03-19 19:38:23 +0530

    Issue resolved.

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -1,12 +1,11 @@
-
 //this file contains the schema for the user based on their role
+
 /*
     This file (`user.js`) defines Mongoose schemas for different user roles (Principal, HoD, Faculty, and Student) in a system. It specifies their required fields and creates models for database storage and retrieval.
 */
 
 /*
 const mongoose = require("mongoose");
-
 //schema for Pricipal
 const principalSchema = new mongoose.Schema({
     name: { type: String },
@@ -14,7 +13,6 @@ const principalSchema = new mongoose.Schema({
     contact: { type: Number, required: true, unique: true },
     password: { type: String, required: true }
 });
-
 //schema for Head of Department
 const hodSchema = new mongoose.Schema({
     name: { type: String },
@@ -23,7 +21,6 @@ const hodSchema = new mongoose.Schema({
     department: { type: String, required: true },
     password: { type: String, required: true }
 });
-
 //schema for Faculty
 const facultySchema = new mongoose.Schema({
     name: { type: String },
@@ -33,7 +30,6 @@ const facultySchema = new mongoose.Schema({
     designation: { type: String, required: true },
     password: { type: String, required: true }
 });
-
 //schema for Student
 const studentSchema = new mongoose.Schema({
     name: { type: String },
@@ -44,17 +40,10 @@ const studentSchema = new mongoose.Schema({
     roll_number: { type: String, required: true, unique: true },
     year: { type: Number, required: true }
 });
-
-
 const Principal = mongoose.model("Principal", principalSchema);
-
 const HoD = mongoose.model("HoD", hodSchema);
-
 const Faculty = mongoose.model("Faculty", facultySchema);
-
 const Student = mongoose.model("Student", studentSchema);
-
-
 module.exports = { Principal, HoD, Faculty, Student };
 */
 
