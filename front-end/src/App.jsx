e0d7b28d32423459834aff8a9d5c8833eadc0445
HemantAAkotkar <163880289+HemantAAkotkar@users.noreply.github.com>
2025-03-19 16:33:55 +0530

    Main Files

---
+++ front-end/src/App.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter ,createBrowserRouter,RouterProvider } from "react-router-dom";
+import About from "/src/Components/About.jsx"
+function App() {
+  return (
+   <About/>
+  )
+}
+
+export default App
