a789de6d0a9dbb44d882815c6f00cc08f7b7b073
Unnati Tekade <unnatitekade546@gmail.com>
2025-03-19 12:06:47 +0530

    Addition of database connection path.

--- back-end/config/dbConnection.js
+++ back-end/config/dbConnection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect(process.env.databaseURL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
